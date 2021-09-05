dc32cd1424a627d5037e28f64b703eb1512dd0ec
baobeta <lequocbao29072001@gmail.com>
2021-09-05 14:42:55 +0700

    comiiit

--- AppTier/DbOrder.cs
+++ AppTier/DbOrder.cs
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 ﻿using DataTier;
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-=======
-﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataTier;
->>>>>>> f00ce9fb19de5e541dc10fd9f1a984b83c32d7db
+
 namespace AppTier
 {
     public class DbOrder
@@ -27,13 +17,6 @@ namespace AppTier
 		{
 			db = new DAL();
 		}
-<<<<<<< HEAD
-		public DataSet getAllOrder()
-		{
-			return db.ExecuteQueryDataSet("select * from orders", CommandType.Text, null);
-		}
-	}
-=======
         public bool insertOrder(ref string err, int customer_id, int seller_id, DateTime sold_date, int total_money)
         {
             return db.MyExecuteNonQuery("sp_InsertOrder", CommandType.StoredProcedure, ref err, new SqlParameter("@customer_id", customer_id), new SqlParameter("@seller_id", seller_id), new SqlParameter("@sold_date", sold_date), new SqlParameter("@total_money", total_money));
@@ -48,5 +31,4 @@ namespace AppTier
         }
     }
    
->>>>>>> f00ce9fb19de5e541dc10fd9f1a984b83c32d7db
 }
